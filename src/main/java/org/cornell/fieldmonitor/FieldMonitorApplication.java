1d17db1f1448e983110c05349d4647a80d44991e
Tinakai Bao <hdsatrewdsaytrew654@gmail.com>
2022-07-11 16:57:04 -0700

    bean-loading test pass

--- src/main/java/org/cornell/fieldmonitor/FieldMonitorApplication.java
+++ src/main/java/org/cornell/fieldmonitor/FieldMonitorApplication.java
@@ -1,13 +1,17 @@
 package org.cornell.fieldmonitor;
 
+import org.cornell.fieldmonitor.config.SpringConfig;
+import org.cornell.fieldmonitor.controller.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class FieldMonitorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FieldMonitorApplication.class, args);
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+        System.out.println(ctx.getBean(UserController.class));
     }
 
 }
