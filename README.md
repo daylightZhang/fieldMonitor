b1b44f77a5a8942a6d12bd549c77efe795b30df9
Jingkai Zhang <jz544@cornell.edu>
2022-07-11 19:04:17 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# fieldMonitor
+this is the project for Sun's lab in Cornell University, Collage of Agriculture and Life Science.
---
+++ src/main/java/org/cornell/fieldmonitor/FieldMonitorApplication.java
@@ -0,0 +1,13 @@
+package org.cornell.fieldmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FieldMonitorApplication.class, args);
+    }
+
+}
