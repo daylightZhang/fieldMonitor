f3c30a076c741574cb8df272ae0aa6f51e091ad7
Tinakai Bao <hdsatrewdsaytrew654@gmail.com>
2022-07-22 21:16:55 -0700

    front:login page and 野獣先輩 update backend:login function implement

--- src/main/java/org/cornell/fieldmonitor/entity/User.java
+++ src/main/java/org/cornell/fieldmonitor/entity/User.java
@@ -1,27 +1,40 @@
 package org.cornell.fieldmonitor.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * 用户信息
  */
 @Data
 public class User implements Serializable {
+    //private static final long serialVersionUID = 1L;
 
-    //姓
-    @TableId(value = "id",type = IdType.AUTO)
-    private Integer id;
+    private Long id;
 
-    private String name;
+    private String username;
 
-    private Integer age;
+    private String password;
 
-    private Integer money;
+    private Integer status;
 
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Long createUser;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateUser;
 
 
 }
