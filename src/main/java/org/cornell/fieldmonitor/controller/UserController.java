838585b6809d7cdd8b62d4c144a08adeeecdc207
Tinakai Bao <hdsatrewdsaytrew654@gmail.com>
2022-07-22 21:16:55 -0700

    front:login page and 野獣先輩 update backend:login function implement

--- src/main/java/org/cornell/fieldmonitor/controller/UserController.java
+++ src/main/java/org/cornell/fieldmonitor/controller/UserController.java
@@ -1,13 +1,16 @@
 package org.cornell.fieldmonitor.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.cornell.fieldmonitor.common.R;
 import org.cornell.fieldmonitor.entity.User;
 import org.cornell.fieldmonitor.service.userService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/user")
@@ -23,13 +26,28 @@ public class UserController {
 //        System.out.print(user.toString() + "success");
 //    }
 
-    @GetMapping("/{id}")
-    public R<User> test(@PathVariable int id){
-        User user = us.getById(id);
-        if(user != null){
-            return R.success(user);
+    @PostMapping("/login")
+    public R<User> login(HttpServletRequest request, @RequestBody User user){
+        String password = user.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+        LambdaQueryWrapper<User> queryWrapper= new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getUsername,user.getUsername());
+        User user1 = us.getOne(queryWrapper);
+
+        if(user1 == null){
+            return R.error("login fall");
         }
-        return R.error("user not found");
+
+        if(!user1.getPassword().equals(password)){
+            return R.error("login fall");
+        }
+
+        if(user1.getStatus() == 0){
+            return R.error("account forbidden");
+        }
+
+        request.getSession().setAttribute("user",user1.getId());
+        return R.success(user1);
     }
 
 
